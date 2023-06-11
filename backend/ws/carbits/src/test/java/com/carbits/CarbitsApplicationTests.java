c6efecab246b6f2731b54e7e4c731f9084988d56
mustafa <mustafaaykurt35@gmail.com>
2023-06-11 14:46:30 +0300

    First Commit

---
+++ backend/ws/carbits/src/test/java/com/carbits/CarbitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
